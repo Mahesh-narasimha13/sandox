af3bd60e364f16b212740b88ec66275098d9a7bd
Mahesh narasimha <polurumahesh13@gmail.com>
2026-04-30 23:14:48 +0530

    app.py

--- app.py
+++ app.py
@@ -64,3 +64,11 @@ def execute_code():
 if __name__ == '__main__':
     # Run the server on port 5000
     app.run(debug=True, port=5000)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
